137b6265935259ff17d8a2a4987a7ff61589631d
Toma <toma.raccoon@gmail.com>
2021-03-20 17:28:22 +0200

    Change js from slick-slader to tiny-slider

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
 	$('.carousel__inner').slick({
 		speed: 1200,
 		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow_left.png"></img></button>',
@@ -13,4 +13,21 @@ $(document).ready(function(){
 			}		
 		]
 	});
+}); */
+
+var slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+	controls: false,
+	nav: false,
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+	slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+	slider.goTo('next');
 });
