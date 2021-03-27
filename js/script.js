e5e3e08dfbb615fdf541690d41882065da59e1a2
Toma <toma.raccoon@gmail.com>
2021-03-27 17:57:51 +0200

    Add comments for script.js how to create SLIDERS and TABS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Вариант слайдеров с помощью jQuery */
 /* $(document).ready(function(){
 	$('.carousel__inner').slick({
 		speed: 1200,
@@ -15,6 +16,7 @@
 	});
 }); */
 
+/* Вариант слайдеров с помощью TinySlider */
 var slider = tns({
     container: '.carousel__inner',
     items: 1,
@@ -32,6 +34,11 @@ document.querySelector('.next').addEventListener('click', function () {
 	slider.goTo('next');
 });
 
+
+
+
+
+/* Переключение табов и их контента c помощью jQuery */
 (function($) {
 	$(function() {
 		
@@ -41,6 +48,27 @@ document.querySelector('.next').addEventListener('click', function () {
 			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
 		});
 		
+/* 		on('click', 'li:not(.catalog__tab_active)' - кликаем на li, c активным классом
+
+		this - ссылается на элемент, на который только что нажали (например на второй таб)
+		
+		Связанное с самими табами
+		.addClass('catalog__tab_active')  - нажали на класс, который не активен и добавили ему класс активности
+		.siblings().removeClass('catalog__tab_active') - все соседние классы удаляют класс активности
+		
+		Связанное с контентом табов
+		.closest('div.container') - находит ближайший элемент
+		find('div.catalog__content') - внутри контейнера находит класс
+		removeClass('catalog__content_active') - внутри элементов, который нашли удаляется активный класс
+		eq($(this).index()) - получает номер элемента, на который мы нажали (например на второй таб)
+		.addClass('catalog__content_active') - и у этого элемента добавляется класс активности
+ */
+
+
+
+
+		
+/* Полный вариант переключения карточек c помощью jQuery */
 		/* $('.catalog-item__link').each(function(i) {
 			$(this).on('click', function(e) {
 				e.preventDefault();
@@ -55,12 +83,30 @@ document.querySelector('.next').addEventListener('click', function () {
 				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
 				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
 			})
-		}); */
+		});  */
+		
+		/* .each(function(i) - для каждого элемента с аргументом і 
+		$(this) - ссылаемся на каждый элемент, который перебирается
+		on('click', function(e) - пользователь кликает на ссылку
+
+		e.preventDefault() - отменяем стандартное поведение браузера, и задаем новое - при клике на ссылку, происходит другое действие (для этого нужно обязательно function(e))
+		$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active') - при клике на кнопку, должен переключаться класс на активный
+		$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active') - тоже самое (но действителен сразу только один класс активности, поскольку в html один класс активный, второй - нет)
+		eq(i) - при клике будут срабатывать не сразу все классы, а именно на который клинули */
+		
 	});
 
-	function toggleSlide(item) {
+
+
+
+
+
+/* Оптимизированый вариант переключения карточек c помощью jQuery (полный вариант выше)
+Вместо item подставляется наш класс
+*/
+	function toggleSlide(item) { 
 		$(item).each(function(i) {
-			$(this).on('click', function(e) {
+			$(this).on('click', function(e) { 
 				e.preventDefault();
 				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
 				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -70,6 +116,5 @@ document.querySelector('.next').addEventListener('click', function () {
 
 	toggleSlide('.catalog-item__link');
 	toggleSlide('.catalog-item__back');
-	
 
 })(jQuery);
